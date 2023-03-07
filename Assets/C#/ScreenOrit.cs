21d155c8c03cbd5a0b0d2c6dfb498068fe749063
WIRIT <118571958+eWIRITe@users.noreply.github.com>
2023-03-07 17:23:47 -0500

    Add screen orientation

---
+++ Assets/C#/ScreenOrit.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenOrit : MonoBehaviour
+{
+    public bool AutoOrient;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (AutoOrient) Screen.orientation = ScreenOrientation.AutoRotation;
+
+        else Screen.orientation = ScreenOrientation.LandscapeLeft;
+    }
+}
